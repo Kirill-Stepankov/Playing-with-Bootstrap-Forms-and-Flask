f8980cb99d0480c3cf7d3ad91aa00adfab7ea442
Kirill Stepankov <kirillstepankov18@gmail.com>
2023-03-13 21:19:55 +0300

    usage of flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 app.config['SECRET_KEY'] = 'rmvmvpwmeedwopcwc'
 
 from . import routes
